4c6a3360d0db8093ea0b9ec4dd38b79f831d13a1
Greg Ziegan <greg.ziegan@gmail.com>
2017-07-10 09:30:01 -0700

    it compiles: style-elements test visualizer

--- src/View.elm
+++ src/View.elm
@@ -4,7 +4,6 @@ import Color exposing (rgb)
 import Element exposing (..)
 import Element.Attributes exposing (..)
 import Html exposing (Html)
-import Html.Attributes as Html
 import String
 import Style exposing (..)
 import Style.Border as Border
@@ -23,10 +22,25 @@ type Styles
     | Main
     | TopBorder
     | TestHeader
+    | TestSummary
+    | TestPanel
+    | Tests
+    | ColoredText TestColor
+    | GivenCode
 
 
-elmBlue =
-    rgb 96 181 204
+type TestColor
+    = FailColor
+    | SucceedColor
+    | TodoColor
+
+
+palette =
+    { primary = rgb 41 60 75
+    , secondary = rgb 84 84 84 -- gray color on elm blog is rgb 221 221 221 but it doesn't meet accessibility standards for contrast http://webaim.org/resources/contrastchecker/
+    , accent = rgb 96 181 204
+    , background = rgb 255 255 255
+    }
 
 
 stylesheet : StyleSheet Styles variation
@@ -34,9 +48,9 @@ stylesheet =
     Style.stylesheet
         [ style None []
         , style TestPane
-            [ Color.text Color.black
-            , Color.background Color.white
-            , Color.border Color.lightGrey
+            [ Color.text palette.primary
+            , Color.background palette.background
+            , Color.border palette.secondary
             ]
         , style TestResult
             []
@@ -45,26 +59,45 @@ stylesheet =
             , Font.typeface [ "Source Sans Pro", "Trebuchet MS", "Lucida Grande", "Bitstream Vera Sans", "Helvetica Neue", "sans-serif" ]
             ]
         , style TopBorder
-            [ Color.background elmBlue
+            [ Color.background palette.accent
             , paddingHint 4
             ]
         , style TestHeader
             [ Color.text Color.red
             , Font.size 20
             ]
+        , style TestSummary
+            [ paddingHint 2 ]
+        , style TestPanel
+            []
+        , style Tests []
+        , style (ColoredText FailColor) [ Color.text Color.red ]
+        , style (ColoredText SucceedColor) [ Color.text Color.green ]
+        , style (ColoredText TodoColor) [ Color.text Color.darkYellow ]
+        , style GivenCode [ Color.text Color.gray ]
         ]
 
 
-testFailures =
-    [ { given = Just "A test", message = "A test does not pass" } ]
 
-
-view _ =
-    ( "#e61212", "Test Run Failed" )
-        |> finished 200 0 testFailures
-        -- |> summarize testFailures
-        |> appContainer
-        |> Element.viewport stylesheet
+--
+-- resultsStyle : Html.Attribute a
+-- resultsStyle =
+--     style
+--         [ ( "font-size", "14px" )
+--         , ( "line-height", "1.3" )
+--         , ( "font-family", "Menlo, Consolas, \"Fira Mono\", \"DejaVu Sans Mono\", \"Liberation Monospace\", \"Liberation Mono\", Monaco, \"Lucida Console\", \"Courier New\", monospace" )
+--         ]
+-- messageAttributes : List (Html.Attribute a)
+-- messageAttributes =
+--     [ width 80
+--     , style
+--         [ ( "margin-left", "32px" )
+--         , ( "margin-bottom", "40px" )
+--         , ( "font-size", "inherit" )
+--         , ( "font-family", "inherit" )
+--         ]
+--     ]
+--
 
 
 appContainer tests =
@@ -83,59 +116,64 @@ topBorder =
     el TopBorder [] Element.empty
 
 
+view : View.Model -> Html a
+view model =
+    Element.viewport stylesheet <|
+        appContainer <|
+            case model of
+                Nothing ->
+                    "Loading Tests..."
+                        |> text
+                        |> el TestHeader []
+                        |> header
+                        |> summarize []
 
---
--- view : View.Model -> Html a
--- view model =
---     case model of
---         Nothing ->
---             h2 [] [ text "Loading Tests..." ]
---                 |> summarize []
---
---         Just ( duration, Runner.Pass passed ) ->
---             ( palette.green, "Test Run Passed" )
---                 |> finished duration passed []
---                 |> summarize []
---
---         Just ( duration, Runner.Todo passed failures ) ->
---             ( palette.yellow, "Test Run Incomplete: TODO's remaining" )
---                 |> finished duration passed failures
---                 |> summarize failures
---
---         Just ( duration, Runner.AutoFail passed Runner.Only ) ->
---             ( palette.yellow, "Test Run Incomplete: Test.only was used" )
---                 |> finished duration passed []
---                 |> summarize []
---
---         Just ( duration, Runner.AutoFail passed Runner.Skip ) ->
---             ( palette.yellow, "Test Run Incomplete: Test.skip was used" )
---                 |> finished duration passed []
---                 |> summarize []
---
---         Just ( duration, Runner.AutoFail passed (Runner.Custom reason) ) ->
---             ( palette.yellow, "Test Run Incomplete: " ++ reason )
---                 |> finished duration passed []
---                 |> summarize []
---
---         Just ( duration, Runner.Fail passed failures ) ->
---             ( palette.red, "Test Run Failed" )
---                 |> finished duration passed failures
---                 |> summarize failures
---
---         Just ( duration, Runner.Running { passed, failures, remaining } ) ->
---             running (passed + List.length failures) remaining
---                 |> summarize failures
---
--- running : Int -> Int -> Html a
--- running completed remaining =
---     div []
---         [ h2 [] [ text "Running Tests..." ]
---         , div [] [ text (toString completed ++ " completed") ]
---         , div [] [ text (toString remaining ++ " remaining") ]
---         ]
--- finished : Time -> Int -> List a -> ( String, String ) -> List (Element Styles variation msg)
+                Just ( duration, Runner.Pass passed ) ->
+                    ( SucceedColor, "Test Run Passed" )
+                        |> finished duration passed []
+                        |> summarize []
+
+                Just ( duration, Runner.Todo passed failures ) ->
+                    ( TodoColor, "Test Run Incomplete: TODO's remaining" )
+                        |> finished duration passed failures
+                        |> summarize failures
+
+                Just ( duration, Runner.AutoFail passed Runner.Only ) ->
+                    ( TodoColor, "Test Run Incomplete: Test.only was used" )
+                        |> finished duration passed []
+                        |> summarize []
+
+                Just ( duration, Runner.AutoFail passed Runner.Skip ) ->
+                    ( TodoColor, "Test Run Incomplete: Test.skip was used" )
+                        |> finished duration passed []
+                        |> summarize []
+
+                Just ( duration, Runner.AutoFail passed (Runner.Custom reason) ) ->
+                    ( TodoColor, "Test Run Incomplete: " ++ reason )
+                        |> finished duration passed []
+                        |> summarize []
+
+                Just ( duration, Runner.Fail passed failures ) ->
+                    ( FailColor, "Test Run Failed" )
+                        |> finished duration passed failures
+                        |> summarize failures
+
+                Just ( duration, Runner.Running { passed, failures, remaining } ) ->
+                    running (passed + List.length failures) remaining
+                        |> summarize failures
 
 
+running : Int -> Int -> Element Styles variations msg
+running completed remaining =
+    column None
+        []
+        [ header <| el TestHeader [] (text "Running Tests...")
+        , row None [] [ text (toString completed ++ " completed") ]
+        , row None [] [ text (toString remaining ++ " remaining") ]
+        ]
+
+
+finished : Time -> Int -> List a -> ( TestColor, String ) -> Element Styles variation msg
 finished duration passed failures ( headlineColor, headlineText ) =
     column None
         [ spacing 10, padding 10 ]
@@ -145,8 +183,7 @@ finished duration passed failures ( headlineColor, headlineText ) =
             [ grid TestResult
                 { rows = [], columns = [] }
                 [ spacing 10 ]
-                [ --area { start = ( 0, 0 ), width = 2, height = 1 }
-                  area { start = ( 0, 0 ), width = 1, height = 1 } (bold "Duration")
+                [ area { start = ( 0, 0 ), width = 1, height = 1 } (bold "Duration")
                 , area { start = ( 1, 0 ), width = 1, height = 1 } (text (formatDuration duration))
                 , area { start = ( 0, 1 ), width = 1, height = 1 } (bold "Passed")
                 , area { start = ( 1, 1 ), width = 1, height = 1 } (text (toString passed))
@@ -157,99 +194,78 @@ finished duration passed failures ( headlineColor, headlineText ) =
         ]
 
 
+summarize : List Runner.Failure -> Element Styles variation msg -> Element Styles variation msg
+summarize failures summary =
+    column TestPanel
+        []
+        [ row None [] [ summary ]
+        , row None [] [ viewFailures failures ]
+        ]
 
--- summarize : List a -> Element Styles variation msg -> Element Styles variation msg
--- summarize failures content =
---     content
--- div
---     [ style
---         [ ( "width", "960px" )
---         , ( "margin", "auto 40px" )
---         , ( "font-family", "verdana, sans-serif" )
---         ]
---     ]
---     [ content
---
---     -- , ol
---     --     [ class "results", resultsStyle ]
---     --     (List.map viewFailure failures)
---     ]
--- viewFailure : Runner.Failure -> Html a
--- viewFailure failure =
---     let
---         ( labels, expectations ) =
---             Runner.formatFailure
---                 (withColorChar '↓' palette.gray)
---                 (withColorChar '✗' palette.red)
---                 failure
---
---         inContext { given, message } =
---             div []
---                 [ Maybe.withDefault (text "") (Maybe.map viewGiven given)
---                 , pre messageAttributes [ text message ]
---                 ]
---     in
---     li
---         [ style [ ( "margin", "40px 0" ) ] ]
---         (labels ++ List.map inContext expectations)
---
---
--- viewGiven : String -> Html a
--- viewGiven value =
---     pre givenAttributes [ text ("Given " ++ value) ]
---
--- givenAttributes : List (Html.Attribute a)
--- givenAttributes =
---     [ width 80
---     , style
---         [ ( "margin-bottom", "24px" )
---         , ( "color", palette.gray )
---         , ( "font-size", "inherit" )
---         , ( "font-family", "inherit" )
---         ]
---     ]
---
---
--- messageAttributes : List (Html.Attribute a)
--- messageAttributes =
---     [ width 80
---     , style
---         [ ( "margin-left", "32px" )
---         , ( "margin-bottom", "40px" )
---         , ( "font-size", "inherit" )
---         , ( "font-family", "inherit" )
---         ]
---     ]
---
--- withColorChar : Char -> String -> String -> Html a
--- withColorChar char textColor str =
---     div [ style [ ( "color", textColor ) ] ]
---         [ text (String.cons char (String.cons ' ' str)) ]
---
---
--- resultsStyle : Html.Attribute a
--- resultsStyle =
---     style
---         [ ( "font-size", "14px" )
---         , ( "line-height", "1.3" )
---         , ( "font-family", "Menlo, Consolas, \"Fira Mono\", \"DejaVu Sans Mono\", \"Liberation Monospace\", \"Liberation Mono\", Monaco, \"Lucida Console\", \"Courier New\", monospace" )
---         ]
+
+viewFailures failures =
+    list Ordered
+        None
+        []
+        (List.map viewFailure failures)
+
+
+type Ordered
+    = Ordered
+    | Unordered
+
+
+list : Ordered -> Styles -> List (Element.Attribute variation msg) -> List (Element Styles variation msg) -> Element Styles variation msg
+list ordered style attrs els =
+    let
+        elements =
+            List.map (node "li") els
+    in
+    case ordered of
+        Ordered ->
+            node "ol" (column None [] elements)
+
+        Unordered ->
+            node "ul" (column None [] elements)
+
+
+viewFailure : Runner.Failure -> Element Styles variations msg
+viewFailure failure =
+    let
+        ( labels, expectations ) =
+            Runner.formatFailure
+                (withTestColor '↓' TodoColor)
+                (withTestColor '✗' FailColor)
+                failure
+
+        inContext { given, message } =
+            column None
+                []
+                [ Maybe.withDefault Element.empty (Maybe.map viewGiven given)
+                , code None [] message
+                ]
+    in
+    row None
+        []
+        (labels ++ List.map inContext expectations)
+
+
+code style attrs str =
+    node "pre" (el style attrs (text str))
+
+
+viewGiven : String -> Element Styles variations msg
+viewGiven value =
+    code GivenCode [] ("Given " ++ value)
+
+
+withTestColor : Char -> TestColor -> String -> Element Styles variation msg
+withTestColor char textColor str =
+    column (ColoredText textColor)
+        []
+        [ text (String.cons char (String.cons ' ' str)) ]
 
 
 formatDuration : Time -> String
 formatDuration time =
     toString time ++ " ms"
-
-
-palette :
-    { green : String
-    , red : String
-    , yellow : String
-    , gray : String
-    }
-palette =
-    { green = "darkgreen"
-    , red = "hsla(3, 100%, 40%, 1.0)"
-    , yellow = "goldenrod"
-    , gray = "darkgray"
-    }
